dd5d4e7bcf67280507d04f406da4b343464ec67a
Lucas Azevedo <164562199+Kinhaz@users.noreply.github.com>
2025-08-28 11:14:10 -0300

    Update questão45.c

--- questão45.c
+++ questão45.c
@@ -4,18 +4,15 @@ int main() {
     char operador;
     float num1, num2, resultado;
 
-    // Solicita o operador
     printf("Digite a operação desejada (+, -, / ou *): ");
-    scanf(" %c", &operador);  // O espaço antes do %c ignora espaços e quebras de linha anteriores
-
-    // Solicita os dois números
+    scanf(" %c", &operador);  
+    
     printf("Digite o primeiro número: ");
     scanf("%f", &num1);
 
     printf("Digite o segundo número: ");
     scanf("%f", &num2);
 
-    // Realiza a operação com base no operador digitado
     switch (operador) {
         case '+':
             resultado = num1 + num2;
