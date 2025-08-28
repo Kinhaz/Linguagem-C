0f51ee525950892dbd8b6eb17262e3df77a7430e
Lucas Azevedo <164562199+Kinhaz@users.noreply.github.com>
2025-08-28 10:20:27 -0300

    Create questão25.c

---
+++ questão25.c
@@ -0,0 +1,13 @@
+int main() {
+    float fahrenhei, celsios;
+    
+    printf("Qual a temperatura fahrenhei: ");
+    scanf ("%f", &fahrenhei);
+    
+    celsios = 5.0 / 9.0 * (fahrenhei - 32);
+    
+    printf("Temperatura em Celsius: %.2f ", celsios);
+    
+    
+    return 0;
+}
